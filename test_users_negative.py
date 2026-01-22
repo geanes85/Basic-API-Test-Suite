dc042c8fda2807bbc919e264033cf16b1627317d
geanes85 <107592034+geanes85@users.noreply.github.com>
2026-01-21 16:06:06 -0800

    Fix formatting in test_get_non_existent_user

--- test_users_negative.py
+++ test_users_negative.py
@@ -1,7 +1,7 @@
 import requests
 
 def test_get_non_existent_user():
-    response = requests.get("https://jsonplaceholder.typicode.com/users/9999")  # ___
+    response = requests.get("https://jsonplaceholder.typicode.com/users/9999")
     assert response.status_code == 404
     data = response.json()
     assert data == {}
